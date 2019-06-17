e82cd317f3859ec3c2a83c38d5b2837ed989fe91
Nicholas Walczak <nwalczak@bbn.com>
2019-06-17 15:27:53 -0500

    spellchecker - update results behaviors for adds
    When a term is added to SBH or the spellchecker, there is a question of
    what to do with the duplicates of that term in the rest of the results.
    Currently, adding to SBH removes all duplicates and adding to the
    spellchecker does nothing.  This behavior should be swapped.

--- intent-parser-server/src/intent_parser_server.py
+++ intent-parser-server/src/intent_parser_server.py
@@ -1243,7 +1243,7 @@ class IntentParserServer:
         buttons = [('Ignore', 'spellcheck_add_ignore'),
                    ('Ignore All', 'spellcheck_add_ignore_all'),
                    ('Add to SynBioHub', 'spellcheck_add_synbiohub'),
-                   ('Add to Dictionary', 'spellcheck_add_dictionary'),
+                   ('Add to Spellchecker Dictionary', 'spellcheck_add_dictionary'),
                    ('Include Previous Word', 'spellcheck_add_select_previous'),
                    ('Include Next Word', 'spellcheck_add_select_next'),
                    ('Remove First Word', 'spellcheck_add_drop_first'),
@@ -1320,10 +1320,7 @@ class IntentParserServer:
 
         actionList = [dialog_action]
 
-        # Since we are adding this term to the SBH dict, we want to ignore any other results
-        self.spellcheck_remove_term(client_state)
-        # Removing the term automatically updates the spelling index
-        #client_state["spelling_index"] += 1
+        client_state["spelling_index"] += 1
         if client_state["spelling_index"] < client_state['spelling_size']:
             for action in self.report_spelling_results(client_state):
                 actionList.append(action)
@@ -1348,7 +1345,10 @@ class IntentParserServer:
         dict_path = os.path.join(self.dict_path, user_id + '.json')
         self.spellCheckers[user_id].export(dict_path)
 
-        client_state["spelling_index"] += 1
+        # Since we are adding this term to the spelling dict, we want to ignore any other results
+        self.spellcheck_remove_term(client_state)
+        # Removing the term automatically updates the spelling index
+        #client_state["spelling_index"] += 1
         if client_state["spelling_index"] >= client_state['spelling_size']:
             # We are at the end, nothing else to do
             return []
