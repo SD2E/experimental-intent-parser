95555c1e668bde50021a6063da6c9e4c5462bac5
Nicholas Walczak <nwalczak@bbn.com>
2019-07-26 14:23:38 -0400

    Need to assign doc_id for generate report

--- intent-parser-server/src/intent_parser_server.py
+++ intent-parser-server/src/intent_parser_server.py
@@ -429,6 +429,7 @@ class IntentParserServer:
 
         client_state = {}
         client_state['doc'] = doc
+        client_state['document_id'] = document_id
         self.analyze_document(client_state, doc, 0)
 
         report = {}
