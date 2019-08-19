711673b4613d99448efba6ea925ce4ced7461029
Nicholas Walczak <nwalczak@bbn.com>
2019-08-19 15:19:07 -0500

    Simple test for create table

--- intent-parser-server/tests/test_unit_ips_spellcheck.py
+++ intent-parser-server/tests/test_unit_ips_spellcheck.py
@@ -415,6 +415,35 @@ class TestIntentParserServer(unittest.TestCase):
             self.assertTrue(add_results['results']['operationSucceeded'], 'Failed on iteration %d' % (it_count))
             it_count += 1
 
+    def test_submit_createMeasurementTable(self):
+        """
+        Test the Create Measurement Table submit action
+        """
+        self.item_types = []
+        self.json_body['data'] = {}
+        self.json_body['data']['isSpellcheck'] = 'False'
+        self.json_body['data']['extra'] = {}
+        self.json_body['data']['extra']['action'] = 'createMeasurementTable'
+        self.json_body['data']['numReagents'] = 3
+        self.json_body['data']['temperature'] = True
+        self.json_body['data']['timepoint'] = False
+        self.json_body['data']['numRows'] = 4
+        self.json_body['data']['selectionStartParagraph'] = 3
+        self.json_body['data']['selectionStartOffset'] = 5
+
+        self.ips.process_submit_form([], [])
+
+        add_results = json.loads(self.ips.send_response.call_args[0][2])
+        actions = add_results['actions']
+
+        self.assertTrue(len(actions) == 1) # Expect one addTable action
+        self.assertTrue(actions[0]['paragraph_index'] == self.json_body['data']['selectionStartParagraph'])
+        self.assertTrue(actions[0]['offset'] == self.json_body['data']['selectionStartOffset'])
+        self.assertTrue(self.json_body['data']['numRows'] + 1 == len(actions[0]['tableData']))
+        self.assertTrue(len(actions[0]['colSizes']) == len(actions[0]['tableData'][0]))
+        self.assertTrue(len(actions[0]['colSizes']) ==  self.json_body['data']['numReagents'] + 3 + 1 + 1)
+
+
     def test_spellcheck_add_select_functions(self):
         """
         Select previous word button action for additions by spelling
