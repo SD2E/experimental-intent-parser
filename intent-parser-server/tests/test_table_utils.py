6eeeda1cc8451e99db227b9e74cb2ccdbb616925
tramyn <tramy.nguy@gmail.com>
2019-12-19 16:00:21 -0500

    test cells without values and units

--- intent-parser-server/tests/test_table_utils.py
+++ intent-parser-server/tests/test_table_utils.py
@@ -45,7 +45,6 @@ class TableUtilsTest(unittest.TestCase):
         for value, unit in tu.transform_cell(cell_str, ['X'], cell_type='fluid'):
            self.assertEqual(unit, 'unspecified')
            self.assertTrue(value in expected_values)  
-           
     
     def testCellWithIncorrectUnitLocation2(self):
         cell_str = '1, 2, X 3'
@@ -53,7 +52,27 @@ class TableUtilsTest(unittest.TestCase):
         for value, unit in tu.transform_cell(cell_str, ['X'], cell_type='fluid'):
            self.assertEqual(unit, 'unspecified')
            self.assertTrue(value in expected_values)   
-           
-                
+    
+    def testCellWithSingleValue(self):
+        cell_str = '1 X'
+        expected_values = ['1']
+        for value, unit in tu.transform_cell(cell_str, ['X'], cell_type='fluid'):
+           self.assertEqual(unit, 'X')
+           self.assertTrue(value in expected_values)       
+
+    def testCellWithNonValues(self):
+        cell_str = 'one, two X'
+        for value, unit in tu.transform_cell(cell_str, ['X'], cell_type='fluid'):
+            self.assertEqual(unit, 'unspecified')
+            self.assertTrue(value == cell_str)
+
+    def testCellNotTypeValueUnit(self):
+        cell_str = 'A simple string'
+        for value, unit in tu.transform_cell(cell_str, ['celsius', 'fahrenheit'], cell_type='temperature'):
+            self.assertEqual(unit, 'unspecified')
+            self.assertTrue(value == cell_str)
+    
+    
+                    
 if __name__ == "__main__":
     unittest.main()
