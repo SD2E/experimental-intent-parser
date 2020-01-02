96eef8d2f7fb86164319496c3763876ce99c3349
tramyn <tramy.nguy@gmail.com>
2020-01-01 17:08:42 -0700

    renamed functions in test_table_utils

--- intent-parser-server/tests/test_table_utils.py
+++ intent-parser-server/tests/test_table_utils.py
@@ -4,69 +4,69 @@ import table_utils as tu
 class TableUtilsTest(unittest.TestCase):
     """Unit test for table_utils class"""
 
-    def testCellWithPropagatedUnit(self):
+    def test_cell_with_propagated_unit(self):
         cell_str = '1 X, 2 X, 3 X'
         expected_values = ['1', '2', '3']
         for value, unit in tu.transform_cell(cell_str, ['X'], cell_type='fluid'):
            self.assertEqual(unit, 'X')
            self.assertTrue(value in expected_values) 
            
-    def testCellWithoutPropagatedUnit(self):
+    def test_cell_without_propagated_unit(self):
         cell_str = '1, 2, 3 X'
         expected_values = ['1', '2', '3']
         for value, unit in tu.transform_cell(cell_str, ['X'], cell_type='fluid'):
            self.assertEqual(unit, 'X')
            self.assertTrue(value in expected_values) 
 
-    def testCellWithoutUnits(self):
+    def test_cell_without_units(self):
         cell_str = '1, 2, 3'
         expected_values = ['1', '2', '3']
         for value, unit in tu.transform_cell(cell_str, ['X'], cell_type='fluid'):
            self.assertEqual(unit, 'unspecified')
            self.assertTrue(value in expected_values) 
 
-    def testCellWithUnitAbbreviation(self):
+    def test_cell_with_unit_abbreviation(self):
         cell_str = '1, 2, 3 fold'
         expected_values = ['1', '2', '3']
         for value, unit in tu.transform_cell(cell_str, ['X'], cell_type='fluid'):
            self.assertEqual(unit, 'X')
            self.assertTrue(value in expected_values)
     
-    def testCellWithUnspecifiedUnit(self):
+    def test_cell_with_unspecified_unit(self):
         cell_str = '1, 2, 3 foo'
         expected_values = ['1', '2', '3']
         for value, unit in tu.transform_cell(cell_str, ['X'], cell_type='fluid'):
            self.assertEqual(unit, 'unspecified')
            self.assertTrue(value in expected_values)  
     
-    def testCellWithIncorrectUnitLocation(self):
+    def test_cell_with_incorrect_unit_location(self):
         cell_str = '1 X, 2, 3'
         expected_values = ['1', '2', '3']
         for value, unit in tu.transform_cell(cell_str, ['X'], cell_type='fluid'):
            self.assertEqual(unit, 'unspecified')
            self.assertTrue(value in expected_values)  
     
-    def testCellWithIncorrectUnitLocation2(self):
+    def test_cell_with_incorrect_unit_location2(self):
         cell_str = '1, 2, X 3'
         expected_values = ['1', '2', '3']
         for value, unit in tu.transform_cell(cell_str, ['X'], cell_type='fluid'):
            self.assertEqual(unit, 'unspecified')
            self.assertTrue(value in expected_values)   
     
-    def testCellWithSingleValue(self):
+    def test_cell_with_single_value(self):
         cell_str = '1 X'
         expected_values = ['1']
         for value, unit in tu.transform_cell(cell_str, ['X'], cell_type='fluid'):
            self.assertEqual(unit, 'X')
            self.assertTrue(value in expected_values)       
 
-    def testCellWithNonValues(self):
+    def test_cell_with_nonvalues(self):
         cell_str = 'one, two X'
         for value, unit in tu.transform_cell(cell_str, ['X'], cell_type='fluid'):
             self.assertEqual(unit, 'unspecified')
             self.assertTrue(value == cell_str)
 
-    def testCellNotTypeValueUnit(self):
+    def test_cell_not_type_value_unit(self):
         cell_str = 'A simple string'
         for value, unit in tu.transform_cell(cell_str, ['celsius', 'fahrenheit'], cell_type='temperature'):
             self.assertEqual(unit, 'unspecified')
