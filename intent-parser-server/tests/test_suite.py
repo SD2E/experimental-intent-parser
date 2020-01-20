4bf804f18111f9875b7882815a96bab4cc77ffd8
tramyn <tramy.nguy@gmail.com>
2020-01-20 11:11:16 -0500

    refactored test_suite to used tests from test_generate_structured_request

--- intent-parser-server/tests/test_suite.py
+++ intent-parser-server/tests/test_suite.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
-
-import unittest, os, sys
-
-try:
-    from intent_parser_server import IntentParserServer
-except Exception as e:
-    sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),'../src'))
-    from intent_parser_server import IntentParserServer
+from test_generate_structured_request import GenerateStruturedRequestTest
+from test_integration_ips import IntegrationIpsTest
+from test_integration_sbh import IntegrationSbhTest 
+from test_measurement_table import MeasurementTableTest
+from test_table_utils import TableUtilsTest 
+from test_unit_ips_analyze_sd2dict import IpsAnalyzeSd2dictTest
+from test_unit_ips_generate import IpsGenerateTest
+from test_unit_ips_spellcheck import IpsSpellcheckTest
+from test_unit_ips_utils import IpsUtilsTest
 
 def suite():
-    loader = unittest.TestLoader()
-    tests_dir = os.path.dirname(os.path.realpath(__file__))
-    suite = loader.discover(tests_dir)
+    suite = unittest.TestSuite()
+    suite.addTest(GenerateStruturedRequestTest)
+    suite.addTest(IntegrationIpsTest)
+    suite.addTest(IntegrationSbhTest)
+    suite.addTest(IpsAnalyzeSd2dictTest)
+    suite.addTest(IpsSpellcheckTest)
+    suite.addTest(IpsUtilsTest)
+    suiteTest(IpsGenerateTest)
+    suite.addTest(MeasurementTableTest)
+    suite.addTest(TableUtilsTest)
     return suite
+
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
