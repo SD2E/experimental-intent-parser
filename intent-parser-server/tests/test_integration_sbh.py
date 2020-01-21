8eecd89dab26dddd76771ec1beb69dbc5f857a1e
tramyn <tramy.nguy@gmail.com>
2020-01-21 13:16:46 -0500

    modified test cases to skip parsing of authentication information

--- intent-parser-server/tests/test_integration_sbh.py
+++ intent-parser-server/tests/test_integration_sbh.py
@@ -34,7 +34,7 @@ class IntegrationSbhTest(unittest.TestCase):
         Configure an instance of IntentParserServer for spellcheck testing.
         """
         # If we don't have the necessary credentials, try reading them in from json
-        if not hasattr(IntegrationSbhTest, 'sbh_username') or not hasattr(IntegrationSbhTest, 'sbh_password'):
+        if not hasattr(IntentParserServer, 'sbh_username') or not hasattr(IntentParserServer, 'sbh_password'):
             with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'sbh_creds.json'), 'r') as fin:
                 creds = json.load(fin)
                 IntegrationSbhTest.sbh_username = creds['username']
