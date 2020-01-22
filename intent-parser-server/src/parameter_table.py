a40b46eaef68ecc3374abae313dfb7a0ba9cfad4
tramyn <tramy.nguy@gmail.com>
2020-01-22 14:23:12 -0500

    modified parameter table to generate string, float, and boolean output values

--- intent-parser-server/src/parameter_table.py
+++ intent-parser-server/src/parameter_table.py
@@ -5,19 +5,35 @@ class ParameterTable(object):
     '''
     Class handles parameter tables 
     '''
-
+    FIELD_WITH_BOOLEAN_VALUE = [constants.PARAMETER_VALIDATE_SAMPLES]
+    FIELD_WITH_FLOAT_VALUE = [constants.PARAMETER_PLATE_READER_INFO_GAIN]
+    
     def __init__(self, parameter_fields={}):
         self._parameter_fields = parameter_fields
     
     def parse_table(self, table):
-        parameter = []
+        parameter_data = {}
         rows = table['tableRows']
         for row in rows[1:]:
-            parameter.append(self._parse_row(rows[0], row))
-        return parameter
+            param_field, param_value = self._parse_row(rows[0], row)
+            # TODO: Support nested result from parameter_value.  
+            if not param_field:
+                continue
+            
+            if param_field in self.FIELD_WITH_FLOAT_VALUE:  
+                parameter_data[param_field] = float(param_value[0])
+            elif param_field in self.FIELD_WITH_BOOLEAN_VALUE:
+                if param_value[0] == 'false':
+                    parameter_data[param_field] = False
+                elif param_value[0] == 'true':
+                    parameter_data[param_field] = True 
+            else:
+                parameter_data[param_field] = param_value[0]
+                    
+        return parameter_data
     
     def _parse_row(self, header_row, row):
-        parameter_data = {}
+        
         content = []
         num_cols = len(row['tableCells'])
         param_field = ''
@@ -32,12 +48,8 @@ class ParameterTable(object):
                 param_field = self._get_parameter_field(cell_txt)
             elif header == constants.COL_HEADER_PARAMETER_VALUE:
                 param_value = table_utils.transform_number_name_cell(cell_txt)
-                
-        if param_field:
-            # TODO: Support nested result from parameter_value.  
-            parameter_data[param_field] = param_value[0]
             
-        return parameter_data 
+        return param_field, param_value 
     
     def _get_parameter_field(self, cell_txt):
         return self._parameter_fields[cell_txt]
