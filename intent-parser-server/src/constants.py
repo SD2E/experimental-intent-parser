2abf85484056a5fabfdafaa261ec90238fd8b7f4
tramyn <tramy.nguy@gmail.com>
2020-01-22 14:22:05 -0500

    add constants related to parameter table

--- intent-parser-server/src/constants.py
+++ intent-parser-server/src/constants.py
@@ -2,7 +2,7 @@
 List of constants used for intent parser server
 """
 
-# String defines for headers in the new-style measurements table
+# String defines for headers in measurements table
 COL_HEADER_MEASUREMENT_TYPE = 'measurement-type'
 COL_HEADER_FILE_TYPE = 'file-type'
 COL_HEADER_REPLICATE = 'replicate'
@@ -15,3 +15,6 @@ COL_HEADER_TIMEPOINT = 'timepoint'
 
 COL_HEADER_PARAMETER = 'Parameter'
 COL_HEADER_PARAMETER_VALUE = 'Value'
+
+PARAMETER_VALIDATE_SAMPLES = 'validate_samples'
+PARAMETER_PLATE_READER_INFO_GAIN = 'plate_reader_info.gain'
