d39002f77de93db6fb3995b64cfbf03de32cb991
tramyn <tramy.nguy@gmail.com>
2020-01-27 11:21:06 -0800

    turn on IpsUtilsTest

--- intent-parser-server/tests/test_unit_ips_utils.py
+++ intent-parser-server/tests/test_unit_ips_utils.py
@@ -22,7 +22,7 @@ except Exception as e:
 
 from google_accessor import GoogleAccessor
 
-@unittest.skip("Skip IPS Utils")
+# @unittest.skip("Skip IPS Utils")
 class IpsUtilsTest(unittest.TestCase):
 
     spellcheckFile = 'doc_1xMqOx9zZ7h2BIxSdWp2Vwi672iZ30N_2oPs8rwGUoTA.json'
