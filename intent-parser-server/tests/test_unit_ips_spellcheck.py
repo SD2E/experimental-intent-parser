b5f56f9b3c40db6d38f287bc0b5be8873b9e9883
tramyn <tramy.nguy@gmail.com>
2020-01-27 11:15:01 -0800

    turn on test_unit_ips_spellcheck

--- intent-parser-server/tests/test_unit_ips_spellcheck.py
+++ intent-parser-server/tests/test_unit_ips_spellcheck.py
@@ -21,7 +21,7 @@ except Exception as e:
 
 from google_accessor import GoogleAccessor
 
-@unittest.skip("Skip IPS Spellcheck")
+# @unittest.skip("Skip IPS Spellcheck")
 class IpsSpellcheckTest(unittest.TestCase):
 
     spellcheckFile = 'doc_1xMqOx9zZ7h2BIxSdWp2Vwi672iZ30N_2oPs8rwGUoTA.json'
