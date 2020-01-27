caf9e81974eca34905f3f3aad78999463ddc6338
tramyn <tramy.nguy@gmail.com>
2020-01-27 11:33:38 -0800

    turn on test_measurement_table

--- intent-parser-server/tests/test_measurement_table.py
+++ intent-parser-server/tests/test_measurement_table.py
@@ -4,7 +4,7 @@ import os
 import table_utils
 import unittest
 
-@unittest.skip("Skip Measurement Table test ")
+# @unittest.skip("Skip Measurement Table test ")
 class MeasurementTableTest(unittest.TestCase):
     '''
        Class to test measurement table
