31b4aaa21d0ecf7c67447a6d1be4437d2ec75747
tramyn <tramy.nguy@gmail.com>
2020-01-31 10:26:53 -0500

    include new strateos terms for parameter table

--- intent-parser-server/src/constants.py
+++ intent-parser-server/src/constants.py
@@ -3,18 +3,23 @@ List of constants used for intent parser server
 """
 
 # String defines for headers in measurements table
-COL_HEADER_MEASUREMENT_TYPE = 'measurement-type'
 COL_HEADER_FILE_TYPE = 'file-type'
-COL_HEADER_REPLICATE = 'replicate'
-COL_HEADER_STRAIN = 'strains'
-COL_HEADER_SAMPLES = 'samples'
-COL_HEADER_ODS = 'ods'
+COL_HEADER_MEASUREMENT_TYPE = 'measurement-type'
 COL_HEADER_NOTES = 'notes'
+COL_HEADER_ODS = 'ods'
+COL_HEADER_REPLICATE = 'replicate'
+COL_HEADER_SAMPLES = 'samples'
+COL_HEADER_STRAIN = 'strains'
 COL_HEADER_TEMPERATURE = 'temperature'
 COL_HEADER_TIMEPOINT = 'timepoint'
 
 COL_HEADER_PARAMETER = 'Parameter'
 COL_HEADER_PARAMETER_VALUE = 'Value'
 
-PARAMETER_VALIDATE_SAMPLES = 'validate_samples'
 PARAMETER_PLATE_READER_INFO_GAIN = 'plate_reader_info.gain'
+PARAMETER_RUN_INFO_ONLY_ENDPOINT_FLOW = 'run_info.only_endpoint_flow'
+PARAMETER_RUN_INFO_READ_EACH_RECOVER = 'run_info.read_each_recovery'
+PARAMETER_RUN_INFO_READ_EACH_INDUCTION = 'run_info.read_each_induction'
+PARAMETER_RUN_INFO_SAVE_FOR_RNASEQ = 'run_info.save_for_rnaseq'
+PARAMETER_RUN_INFO_SKIP_FIRST_FLOW = 'run_info.skip_first_flow'
+PARAMETER_VALIDATE_SAMPLES = 'validate_samples'
