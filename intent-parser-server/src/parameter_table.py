1b20a0cd3854dbf73ee24349c7cc512d5a0230ab
tramyn <tramy.nguy@gmail.com>
2020-02-14 13:00:47 -0500

    handle catch exceptions for parameter table

--- intent-parser-server/src/parameter_table.py
+++ intent-parser-server/src/parameter_table.py
@@ -1,4 +1,4 @@
-from intent_parser_exceptions import TableException
+from intent_parser_exceptions import TableException, DictionaryMaintainerException
 import constants
 import logging
 import table_utils
@@ -27,30 +27,38 @@ class ParameterTable(object):
         rows = table['tableRows']
         for row in rows[1:]:
             param_field, param_value = self._parse_row(rows[0], row)
-            if not param_field:
+            if not param_field or len(param_value) == 0:
                 continue
-            # TODO: raise exception for empty parameter value?
-            if len(param_value) == 0:
-                continue
-            elif len(param_value) == 1:
-                key, value = self._parse_parameter_field_value(param_field, param_field, param_value[0])
-                parameter_data[key] = value
-            else:
-                for index in range(len(param_value)):
-                    param_field_id = '.'.join([param_field, str(index)])
-                    key, value = self._parse_parameter_field_value(param_field, param_field_id, param_value[index])
+            try:
+                if len(param_value) == 1:
+                    key, value = self._parse_parameter_field_value(param_field, param_field, param_value[0])
                     parameter_data[key] = value
-                    
+                else:
+                    for index in range(len(param_value)):
+                        param_field_id = '.'.join([param_field, str(index)])
+                        key, value = self._parse_parameter_field_value(param_field, param_field_id, param_value[index])
+                        parameter_data[key] = value
+            except ValueError as value_err:
+                message = str(value_err)
+                self._logger.info('WARNING ' + message)
+                self._validation_errors.append(message)       
+            except TableException as table_err:
+                message = ' '.join([table_err.get_expression(), table_err.get_message()]) 
+                self._logger.info('WARNING ' + message)
+                self._validation_errors.append(message) 
         return parameter_data
     
     def _parse_parameter_field_value(self, parameter_field, parameter_field_id, parameter_value):
-        if parameter_field in self.FIELD_WITH_FLOAT_VALUE:  
+        if parameter_field in self.FIELD_WITH_FLOAT_VALUE: 
             return parameter_field_id, float(parameter_value)
         elif parameter_field in self.FIELD_WITH_BOOLEAN_VALUE:
+            parameter_value = parameter_value.lower()
             if parameter_value == 'false':
                 return parameter_field_id, False
             elif parameter_value == 'true':
                 return parameter_field_id, True
+            else:
+                raise TableException(parameter_value, 'is not a boolean value')
         
         return parameter_field_id, parameter_value
         
@@ -69,16 +77,22 @@ class ParameterTable(object):
                     param_field = self._get_parameter_field(cell_txt)
                 elif header == constants.COL_HEADER_PARAMETER_VALUE:
                     param_value = table_utils.transform_number_name_cell(cell_txt)
-                
+            
+            if not param_field:
+                raise TableException('Parameter field', 'is empty') 
             return param_field, param_value 
         except TableException as err:
-            message = ' '.join(['In Parameter Table: ', err.get_expression(), err.get_message(), 'for', cell_txt]) 
+            message = ' '.join(['In Parameter Table: ', err.get_expression(), err.get_message()]) 
             self._logger.info('WARNING ' + message)
             self._validation_errors.append(message)
             
     def _get_parameter_field(self, cell_txt):
+        if not self._parameter_fields:
+            raise DictionaryMaintainerException('Strateos mapping', 'is empty')
         if not cell_txt in self._parameter_fields:
-            raise TableException(cell_txt, 'is a parameter field not supported for Strateos')
+            raise TableException(cell_txt, 'does not map to a Strateos UID')
         return self._parameter_fields[cell_txt]
-            
+    
+    def get_validation_errors(self):
+        return self._validation_errors      
         
