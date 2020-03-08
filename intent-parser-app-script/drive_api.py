9c8b8d0b6cf088abf70cb980cc4dbc835d3132d6
tramyn <tramy.nguy@gmail.com>
2020-03-08 18:32:14 -0400

    update script to run from logged files

--- intent-parser-app-script/drive_api.py
+++ intent-parser-app-script/drive_api.py
@@ -9,39 +9,51 @@ class DriveAPI(object):
 
     def __init__(self, creds):
         self._service = build('drive', 'v3', credentials=creds)
+
     
-    def get_documents_from_drive(self, drive_id):
-        results = self._service.files().list(
-            q="'%s' in parents and mimeType='application/vnd.google-apps.document'" % (drive_id,),
-            spaces='drive',
-            pageSize=1000,
-            fields='nextPageToken, files(id, name)').execute()
-        documents = results.get('files', [])
-        return documents
-    
-    def get_folders_from_drive(self, drive_id):
-        results = self._service.files().list(
-            q="'%s' in parents and mimeType='application/vnd.google-apps.folder'" % (drive_id,),
-            spaces='drive',
-            pageSize=1000,
-            fields='nextPageToken, files(id, name)').execute()
-        directories = results.get('files', [])
-        return directories
-    
-    def recursive_list_doc(self, drive_id):
-        doc_list = self._recursive_doc(drive_id, [])
-        print('Found %s Google Docs' % len(doc_list))
-        return [doc['id'] for doc in doc_list]
+    def get_documents_from_folder(self, folder_id):
+        '''
+        Get all documents within a Google Drive folder.
         
-    def _recursive_doc(self, drive_id, doc_list): 
-        print('Processing folder: ' + drive_id)
-        folder_list = self.get_folders_from_drive(drive_id)
+        Args:
+            folder_id: Google Drive folder id
+        
+        Returns:
+             A list of ids for  all files located within a Google Drive folder
+        ''' 
+        results = self._service.files().list(
+            q="'%s' in parents and mimeType='application/vnd.google-apps.document'" % (folder_id,),
+            spaces='drive',
+            pageSize=1000,
+            fields='nextPageToken, files(id, name)').execute()
+        docs_dict = results.get('files', [])
+        doc_list = [doc['id'] for doc in docs_dict]
+        return docs_dict
+    
+    def get_subfolders_from_folder(self, folder_id):
+        results = self._service.files().list(
+            q="'%s' in parents and mimeType='application/vnd.google-apps.folder'" % (folder_id,),
+            spaces='drive',
+            pageSize=1000,
+            fields='nextPageToken, files(id, name)').execute()
+        folder_dict = results.get('files', [])
+        folder_list = [folder['id'] for folder in folder_dict]
+        return folder_dict
+    
+    def recursive_list_doc(self, folder_id):
+        doc_list = self._recursive_doc(folder_id, [])
+        print('Found %s Google Docs' % len(doc_list))
+        return doc_list 
+        
+    def _recursive_doc(self, folder_id, doc_list): 
+        print('Processing folder: ' + folder_id)
+        folder_list = self.get_subfolders_from_folder(folder_id)
         if not folder_list:
-            doc_ids = self.get_documents_from_drive(drive_id)
+            doc_ids = self.get_documents_from_folder(folder_id)
             doc_list.extend(doc_ids)  
             return doc_list
         
-        doc_ids = self.get_documents_from_drive(drive_id)
+        doc_ids = self.get_documents_from_folder(folder_id)
         doc_list.extend(doc_ids)  
         
         for dir in folder_list:
