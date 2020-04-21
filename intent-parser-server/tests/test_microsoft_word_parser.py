aac5cc5516b8ef1962377137120593c36309766c
tramyn <tramy.nguy@gmail.com>
2020-04-21 15:26:14 -0700

    set up test cases for parsing microsoft word

---
+++ intent-parser-server/tests/test_microsoft_word_parser.py
@@ -0,0 +1,42 @@
+from docx import Document
+from microsoft_word_parser import MicrosoftWordParser
+import os
+import unittest
+
+
+class MicrosoftWordParserTest(unittest.TestCase):
+
+
+    def setUp(self):
+        curr_path = os.path.dirname(os.path.realpath(__file__))
+        self.file_name = 'Nick Copy of CP Experimental Request - NovelChassisYeastStates_TimeSeries'
+        file = '%s.docx' % self.file_name
+        file_path = os.path.join(curr_path, 'data', file)
+        document = Document(file_path)
+        self.doc_parser = MicrosoftWordParser(document)
+
+    def tearDown(self):
+        pass
+
+
+    def test_parse_document_title(self):
+        self.assertEquals(self.doc_parser.title(), self.file_name)
+        
+    def test_get_current_revision(self):
+        self.assertEquals(self.doc_parser.revision(), 1)
+        
+    def test_getting_paragraphs(self):
+        list_of_paragraphs = self.doc_parser.paragraphs()
+    
+    def test_tables(self):
+        self.doc_parser.tables() 
+        
+    def test_link_info(self):
+        linked_words = self.doc_parser.link_info()
+        for word in linked_words:
+            print('%s : %s' % (word, linked_words[word]))
+        print(len(linked_words))
+
+
+if __name__ == "__main__":
+    unittest.main()
