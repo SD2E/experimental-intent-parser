33c177920b4aa0159d13efca020aa44da9b23bbd
tramyn <tramy.nguy@gmail.com>
2020-04-21 15:27:07 -0700

    removed irrelevant test case

--- intent-parser-app-script/tests/test_google_drive_api.py
+++ intent-parser-app-script/tests/test_google_drive_api.py
@@ -1,6 +1,7 @@
 from google_drive_accessor import GoogleDriveAccessor
 from google_drive_accessor_v2 import GoogleDriveAccessorV2
 import ip_addon_script
+import json
 import unittest
 
 
@@ -30,35 +31,7 @@ class DriveAPITest(unittest.TestCase):
     def test_recursive_list_folders(self):
         response = self.drive_api.get_recursive_folders(self.folder_id)
         self.assertEquals(1, len(response))
-        
-    def test_print_doc_revisions(self):
-        file_id = '1xMqOx9zZ7h2BIxSdWp2Vwi672iZ30N_2oPs8rwGUoTA'
-        list_of_revisions = self.drive_api.list_revision(file_id)
-        print(list_of_revisions)
-    
-    def test_download_doc(self):
-        file_id = '1xMqOx9zZ7h2BIxSdWp2Vwi672iZ30N_2oPs8rwGUoTA'
-        response = self.drive_api.download_file(file_id)
-        
-        with open('temp2.pdf', 'wb') as output:
-            output.write(response)
-            
-    def test_download_file_with_revision(self):
-        file_id = '1xMqOx9zZ7h2BIxSdWp2Vwi672iZ30N_2oPs8rwGUoTA'
-        response = self.drive_api.download_file_with_revision('temp_file', file_id, '1976', 'html')
-    
-    def test_export_doc(self):
-        file_id = '1xMqOx9zZ7h2BIxSdWp2Vwi672iZ30N_2oPs8rwGUoTA'
-        response = self.drive_api.export_file(file_id)
-       
-    def test_drivev2_list_revisions(self):
-        file_id = '1xMqOx9zZ7h2BIxSdWp2Vwi672iZ30N_2oPs8rwGUoTA'
-        drive_v2 = GoogleDriveAccessorV2(self.creds)
-        result = drive_v2.list_file_revision(file_id)
-        export_options = result[0]['exportLinks']
-        html_export_url = export_options['text/html']
-        
-        print(html_export_url)
+  
         
 if __name__ == "__main__":
     unittest.main()
