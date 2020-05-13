865431f04d19e25099649f71962cf6f020fb5940
tramyn <tramy.nguy@gmail.com>
2020-05-13 13:27:49 -0700

    modified lab table to parse experiment_id

--- intent_parser/table/lab_table.py
+++ intent_parser/table/lab_table.py
@@ -1,24 +1,75 @@
+from intent_parser.intent_parser_exceptions import TableException
 import intent_parser.utils.intent_parser_utils as intent_parser_utils
+import intent_parser.table.table_utils as table_utils
 import logging
 
 class LabTable(object):
-    '''
+    """
     Class for parsing Lab table
-    '''
-    
-    _logger = logging.getLogger('src')
+    """
+    EXPERIMENT_ID_PREFIX = 'experiment'
+    _logger = logging.getLogger('intent_parser')
 
     def __init__(self):
-        pass
+        self._lab_content = {}
+        self._validation_errors = []
+        self._validation_warnings = []
        
     def parse_table(self, table):
         rows = table['tableRows']
-        numRows = len(rows)
-        labRow = rows[0]
-        numCols = len(labRow['tableCells'])
-        if numRows > 1 or numCols > 1:
-            self._logger.info('WARNING: Lab table size differs from expectation! Expecting 1 row and 1 col, found %d rows and %d cols' % (numRows, numCols))
+        for row in rows:
+            self._parse_row(row)
         
-        # The lab text is expected to be in row 0, col 0 and have the form: Lab: <X>
-        lab = intent_parser_utils.get_paragraph_text(labRow['tableCells'][0]['content'][0]['paragraph']).strip().split(sep=':')[1].strip()
-        return lab
+        result = {}
+        if 'lab' not in self._lab_content:
+            result['lab'] = 'tacc'
+            self._validation_errors('Lab table is missing a lab name')
+        else:
+            result['lab'] = self._lab_content['lab']
+        
+        if 'experiment_id' not in self._lab_content:
+            result['experiment_id'] = 'experiment.%s.TBD' % result['lab']
+        else:
+            result['experiment_id'] = 'experiment.%s.%s' % (result['lab'], self._lab_content['experiment_id'])
+        return result
+    
+    def get_validation_errors(self):
+        return self._validation_errors
+    
+    def get_validation_warnings(self):
+        return self._validation_warnings
+    
+    def _parse_row(self, row):
+        cells = row['tableCells']
+        for i in range(len(cells)): 
+            cell_content = intent_parser_utils.get_paragraph_text(cells[i]['content'][0]['paragraph']).strip()
+            if self._is_lab(cell_content.lower()):
+                self._parse_lab(cell_content)
+            elif self._is_experiment_id(cell_content.lower()):
+                self._parse_experiment_id(cell_content)
+            
+    def _is_lab(self, cell_content):
+        return cell_content.startswith('lab')
+        
+    def _parse_lab(self, cell_content):
+        try:
+            lab_name = table_utils.extract_name_from_str(cell_content, 'lab:')
+            if not lab_name:
+                raise TableException('lab name is empty.')
+            self._lab_content['lab'] = lab_name
+        except (ValueError, TableException) as err:
+            self._validation_errors.append('Lab table has invalid %s value: %s' % ('lab name', err.get_message()))
+    
+    def _is_experiment_id(self, cell_content):
+        return cell_content.startswith('experiment_id')
+   
+    def _parse_experiment_id(self, cell_content):
+        try:
+            experiment_id = table_utils.extract_name_from_str(cell_content, 'experiment_id:')
+            if not experiment_id:
+                experiment_id = 'TBD'
+            self._lab_content['experiment_id'] = experiment_id
+        except (ValueError, TableException) as err:
+            self._lab_content['experiment_id'] = 'TBD' 
+            self._validation_warnings.append('Lab table has invalid %s value: %s' % ('experiment_id', err.get_message()))
+    
