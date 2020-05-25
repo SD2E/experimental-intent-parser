6eac693a4c735bc278daf6d09dc536186c04463e
tramyn <tramy.nguy@gmail.com>
2020-05-25 15:09:32 -0700

    process controls table using intent_parser_table

--- intent_parser/table/intent_parser_cell.py
+++ intent_parser/table/intent_parser_cell.py
@@ -10,10 +10,13 @@ class IntentParserCell(object):
     def add_paragraph(self, content, link=None):
         self.paragraphs.append(self.Paragraph(content, link))
         
-    def get_content(self):
+    def get_text(self):
         flatten = [p.paragraph for p in self.paragraphs]
         return ''.join(flatten)
-      
+    
+    def get_text_with_url(self):
+        return {p.paragraph: p.link for p in self.paragraphs}
+        
     class Paragraph(object):
         def __init__(self, paragraph, link):
             self.paragraph = paragraph
--- intent_parser/table/intent_parser_table.py
+++ intent_parser/table/intent_parser_table.py
@@ -1,25 +1,25 @@
 from intent_parser.intent_parser_exceptions import TableException
-from intent_parser.table.intent_parser_cell import IntentParserCell
+
 class IntentParserTable(object):
     """
     
     """
     def __init__(self):
-       self._rows = [] 
+        self._rows = [] 
 
     def add_row(self, row):
         self._rows.append(row)
        
     def get_cell(self, row_index, col_index):  
-        if row_index not in self._rows or col_index not in self._cols:
-            raise IndexError('Cannot access cell (%s, %s)' % row_index, col_index)
+        row = self.get_row(row_index)
+        if col_index < 0 or col_index >= len(row):
+            raise IndexError('Cannot access cell (%s, %s)' % (row_index, col_index))
         return self._rows[row_index][col_index] 
     
     def get_row(self, row_index):
-        rows = self._table_parser['tableRows']
-        if row_index < 0 or row_index >= len(rows):
-            raise TableException('Index out of bound')
-        return rows[row_index]
+        if row_index < 0 or row_index >= len(self._rows):
+            raise IndexError('Index out of bound')
+        return self._rows[row_index]
     
     def number_of_rows(self):
         return len(self._rows)  
