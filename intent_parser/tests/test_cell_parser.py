b266703b5097cd4b9986a13234ef63821981ad9b
tramyn <tramy.nguy@gmail.com>
2020-06-09 23:19:13 -0700

    use tokens to parse table headers

--- intent_parser/tests/test_cell_parser.py
+++ intent_parser/tests/test_cell_parser.py
@@ -10,6 +10,30 @@ class CellParserTest(unittest.TestCase):
     def tearDown(self):
         pass
     
+    def test_reagent_header_without_timepoint(self):
+        cell = IntentParserCell()
+        cell.add_paragraph('name1')
+        name, _ = self.parser.process_reagent_header(cell, units={'hour'}, unit_type='timepoints')
+        self.assertEqual('name1', name['label'])
+    
+    def test_reagent_alphanumeric_header_with_timepoint(self):
+        cell = IntentParserCell()
+        cell.add_paragraph('BE1 @ 15 hours')
+        name, timepoint = self.parser.process_reagent_header(cell, units={'hour'}, unit_type='timepoints')
+        self.assertEqual('BE1', name['label'])
+        self.assertEqual('NO PROGRAM DICTIONARY ENTRY', name['sbh_uri'])
+        self.assertEqual(timepoint['value'], 15.0)
+        self.assertEqual(timepoint['unit'], 'hour')
+            
+    def test_reagent_header_with_timepoint(self):
+        cell = IntentParserCell()
+        cell.add_paragraph('name @ 15 hours')
+        name, timepoint = self.parser.process_reagent_header(cell, units={'hour'}, unit_type='timepoints')
+        self.assertEqual('name', name['label'])
+        self.assertEqual('NO PROGRAM DICTIONARY ENTRY', name['sbh_uri'])
+        self.assertEqual(timepoint['value'], 15.0)
+        self.assertEqual(timepoint['unit'], 'hour')
+        
     def test_parse_content_item_with_name(self):
         cell = IntentParserCell()
         cell.add_paragraph('name')
