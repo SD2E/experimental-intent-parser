9652a5bf7c6549992e8520bd1fb8a062989742a7
tramyn <tramy.nguy@gmail.com>
2020-09-02 16:35:52 -0600

    created class to analyze document

---
+++ intent_parser/document/analyze_document.py
@@ -0,0 +1,20 @@
+import threading
+
+class AnalyzeDocument(object):
+
+    def __init__(self, http_message):
+        self.analyze_processing_map_lock = threading.Lock()
+        self.analyze_processing_map = {}
+        self.analyze_thread = threading.Thread(target=self._initiate_document_analysis,
+                                               args=(http_message,)  # without comma you'd get a... TypeError
+        )
+
+    def start_document_analysis(self):
+        self._initiate_document_analysis()
+        self.analyze_thread.start()
+
+    def stop_document_analysis(self):
+        self.analyze_thread.join()
+
+    def _initiate_document_analysis(self):
+        pass
